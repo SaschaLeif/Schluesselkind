01ee2ce0c63638f882bc83c5f21d17fcab958cda
Leif Sascha <leif.timm@studen.htw-berlin.de>
2012-09-17 19:11:41 +0200

    lalala

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Schluesselkind::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #false -> Heroku test
 
   # Generate digests for assets URLs
   config.assets.digest = true

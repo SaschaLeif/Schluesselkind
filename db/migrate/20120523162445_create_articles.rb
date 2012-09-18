fa84e596c8e35ba088fe6ef208946ce6a92849ea
Leif Sascha <leif.timm@studen.htw-berlin.de>
2012-09-19 00:24:52 +0200

    vergessen..

--- db/migrate/20120523162445_create_articles.rb
+++ db/migrate/20120523162445_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :name
       t.string :article_url
-      #t.string :gender
       t.timestamps
     end
   end
